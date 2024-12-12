3d0f4db7631b131fcdfb3eb5bdc6ce460c4070cf
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-12-12 18:00:55 -0500

    fix: typo, add logging

--- src/processor.ts
+++ src/processor.ts
@@ -2,20 +2,23 @@ import { EigenPodProcessor } from './types/eth/eigenpod.js'
 import { EthChainId, EthContext } from "@sentio/sdk/eth";
 
 
-const address = '0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338D' // eigenpod mainnet contract
+const address = '0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338' // eigenpod mainnet contract
 const start_block = 17445564
 
 EigenPodProcessor.bind({
   address: address,
-  // network: EthChainId.ETHEREUM,
+  network: EthChainId.ETHEREUM,
   startBlock: start_block
 }).onCallVerifyWithdrawalCredentials(async (call, ctx: EthContext) => {
+  console.log("Processing call:", call);
+  
   if (call.error) {
+    console.log("Call error detected");
     return;
   }
 
   // Log the details of the verification call
-  ctx.eventLogger.emit("Withdrawal Credentials Verification", {
+  ctx.eventLogger.emit("verify_withdraw_credentials", {
     beaconTimestamp: call.args.beaconTimestamp.toString(),
     numValidators: call.args.validatorIndices.length,
     stateRootProof: call.args.stateRootProof,
