18f6913bb912fb08bcf6356d404d2158a3366c04
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-12-16 15:49:15 -0500

    feat: add mevcommit avs validator registered event

---
+++ src/mevCommitAVSProcessor.ts
@@ -0,0 +1,13 @@
+import { MevCommitAVSProcessor } from "./types/eth/mevcommitavs.js";
+
+export function initMevCommitAVSProcessor() {
+    return MevCommitAVSProcessor.bind({
+        address: '0xBc77233855e3274E1903771675Eb71E602D9DC2e'
+    })
+    .onEventValidatorRegistered(async (event, ctx) => {
+        ctx.eventLogger.emit("mevcommit_avs_validator_registered", {
+            podOwner: event.args.podOwner,
+            validatorPubKey: event.args.validatorPubKey
+        });
+    });
+}
--- src/processor.ts
+++ src/processor.ts
@@ -1,6 +1,10 @@
 import { initEigenPodManagerEventsProcessor } from './eigenPodProcessor.js'
 import { initEigenPodBeaconProxyProcessor } from './beaconProxyProcessor.js'
+import { initMevCommitAVSProcessor } from './mevCommitAVSProcessor.js'
 
-
+// eigenpod events
 initEigenPodManagerEventsProcessor()
 initEigenPodBeaconProxyProcessor()
+
+// mev-commit avs validator registration events
+initMevCommitAVSProcessor()
