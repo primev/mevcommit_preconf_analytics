cef5ade19fa1271b731ad91d1d63aabf3ec69e0e
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-02-25 12:16:59 -0800

    bid volume per l1 block queries

---
+++ sentio_queries/bid_volume_per_l1_block_testnet.sql
@@ -0,0 +1,28 @@
+SELECT
+    b.block_number AS l1_block_number,
+    b.block_timestamp AS l1_block_timestamp,
+    SUM(o.bidAmt) / 1e18 AS total_bid_volume_eth
+FROM 
+(
+    SELECT
+        block_number,
+        block_timestamp,
+        toUInt64(toUnixTimestamp(block_timestamp) / 12) AS block_bucket
+    FROM ethereum.blocks
+) AS b
+LEFT JOIN 
+(
+    SELECT
+        bidAmt,
+        timestamp,
+        toUInt64(toUnixTimestamp(timestamp) / 12) AS block_bucket
+    FROM preconf_manager_opened_commitments
+    WHERE chain = '1088'
+) AS o
+ON b.block_bucket = o.block_bucket
+GROUP BY
+    b.block_number,
+    b.block_timestamp
+HAVING SUM(o.bidAmt) / 1e18 > 0
+ORDER BY
+    b.block_number DESC;
